0841f5e5889c117fd8cf39e337085ca441cd977a
Konstantin S. <havendv@gmail.com>
2026-05-24 07:52:11 +0400

    feat: Updated OpenAPI spec (#23)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Loudly/Generated/Loudly.CatalogClient.GetCatalogTags.g.cs
+++ src/libs/Loudly/Generated/Loudly.CatalogClient.GetCatalogTags.g.cs
@@ -342,18 +342,17 @@ namespace Loudly
                                     __exception_404 = __ex;
                                 }
 
-                                throw new global::Loudly.ApiException<global::Loudly.Error>(
+
+                                throw global::Loudly.ApiException<global::Loudly.Error>.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_404 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_404,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_404,
-                                    ResponseObject = __value_404,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_404,
+                                    responseObject: __value_404,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                             if (__effectiveReadResponseAsString)
@@ -387,17 +386,15 @@ namespace Loudly
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::Loudly.ApiException(
+                                    throw global::Loudly.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -434,17 +431,15 @@ namespace Loudly
                                     {
                                     }
 
-                                    throw new global::Loudly.ApiException(
+                                    throw global::Loudly.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
