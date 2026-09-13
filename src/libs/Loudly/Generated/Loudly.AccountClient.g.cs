94b258d55b9a7e90f9d8d0ea03fff2c243a0878a
Konstantin S. <havendv@gmail.com>
2026-09-13 19:48:24 +0400

    feat: Updated OpenAPI spec (#42)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Loudly/Generated/Loudly.AccountClient.g.cs
+++ src/libs/Loudly/Generated/Loudly.AccountClient.g.cs
@@ -34,7 +34,7 @@ namespace Loudly
         /// <inheritdoc/>
         public global::Loudly.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Loudly.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Loudly.AccountSourceGenerationContext.Default);
 
         /// <summary>
         ///
